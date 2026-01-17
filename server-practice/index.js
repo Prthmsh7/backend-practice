d4cd1456160cffbd56071ba57e6e6140e6210cbd
Prathmesh Shukla <Prathmeshshukla2106@gmail.com>
2026-01-17 15:44:47 +0530

    d-4

--- server-practice/index.js
+++ server-practice/index.js
@@ -1,5 +1,9 @@
 // this file is the entry point of our server
 const http = require('http');
-const myServer = http.createServer();
+const myServer = http.createServer((req,res)=>{
+    console.log("New Request Recieved");
+    res.end("Hello This is my Server");
+});
 
+myServer.listen(8000,()=> console.log("Server Started"));
 
