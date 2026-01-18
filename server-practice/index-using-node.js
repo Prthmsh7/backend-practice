92461d2fcef42bf098ee93b7c480345a4cf0c817
Prathmesh Shukla <Prathmeshshukla2106@gmail.com>
2026-01-18 18:39:52 +0530

    d6

---
+++ server-practice/index-using-node.js
@@ -0,0 +1,35 @@
+// this file is the entry point of our server
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+// This is a http server completely written from scratch in Node
+// Express Js does the exact same thing for us
+// It basically writes the entire myHandler Function for us
+function MyHandler(req,res)=>{
+    if(req.url==="/favicon.ico") return res.end();
+    const log = `${Date.now()}:${req.method} ${req.url} New Request Received \n`;
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
+    fs.appendFile("log.txt",log,(err,data)=>{
+        
+        switch(myUrl.pathname){
+            case "/":
+                res.end("HomePage");
+                break;
+            case "/about":
+                const username = myUrl.query.myname;
+                res.end(`Hi, ${username}`);
+                break;
+            case "/signup":
+                if(req.method==="GET") res.end('This is a signup form');
+                else if(req.method==="POST") res.end("Success");
+            default:
+                res.end("404 Not Found");
+    };
+});
+
+}
+const myServer = http.createServer(MyHandler);
+myServer.listen(8000,()=> console.log("Server Started"));
+
--- server-practice/index.js
+++ server-practice/index.js
@@ -1,9 +1,42 @@
 // this file is the entry point of our server
 const http = require('http');
-const myServer = http.createServer((req,res)=>{
-    console.log("New Request Recieved");
-    res.end("Hello This is my Server");
+const fs = require('fs');
+const url = require('url');
+const express = require('express');
+
+const app = express();
+app.get('/',(req,res)=>{
+    return res.send("Hello from express");
+})
+
+
+// This is a http server completely written from scratch in Node
+// Express Js does the exact same thing for us
+// It basically writes the entire myHandler Function for us
+function MyHandler(req,res)=>{
+    if(req.url==="/favicon.ico") return res.end();
+    const log = `${Date.now()}:${req.method} ${req.url} New Request Received \n`;
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
+    fs.appendFile("log.txt",log,(err,data)=>{
+        
+        switch(myUrl.pathname){
+            case "/":
+                res.end("HomePage");
+                break;
+            case "/about":
+                const username = myUrl.query.myname;
+                res.end(`Hi, ${username}`);
+                break;
+            case "/signup":
+                if(req.method==="GET") res.end('This is a signup form');
+                else if(req.method==="POST") res.end("Success");
+            default:
+                res.end("404 Not Found");
+    };
 });
 
+}
+const myServer = http.createServer(MyHandler);
 myServer.listen(8000,()=> console.log("Server Started"));
 
