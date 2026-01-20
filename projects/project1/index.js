accf923a63a4b1fa30173473a28f9e490d0cdfb1
Prathmesh Shukla <Prathmeshshukla2106@gmail.com>
2026-01-20 15:04:26 +0530

    task-1

--- projects/project1/index.js
+++ projects/project1/index.js
@@ -1,8 +1,12 @@
 const express = require ('express')
 const users = require('./MOCK_DATA.json');
+const fs = require('fs');
 
 const app = express();
 const PORT = 8000;
+
+//Middleware - Think of this as a plugin
+app.use(express.urlencoded({ extended:false}))
 
 // REST API
 
@@ -19,19 +23,6 @@ app.get('/users',(req,res)=>{
 // Client side rendering
 app.get('/api/users',(req,res)=>{
     return res.json(users);
-})
-
-app.route("/api/users/:id")
-.get((req,res)=>{
-    const id = Number(req.params.id);
-    const user = users.find(user => user.id === id);
-    return res.json(user);
-})
-.patch((req,res)=>{
-    return res.json({status:"pending"});
-})
-.delete((req,res)=>{
-    return res.json({status:"pending"});
 })
 
 // We merged and groupedthese in the above step using .route
@@ -51,9 +42,39 @@ app.route("/api/users/:id")
 //     return res.json({status: "pending"});
 // });
 
+app.route("/api/users/:id")
+.get((req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    return res.json(user);
+})
+.patch((req,res)=>{
+    const id = Number(req.params.id);
+    const body = req.body;
+    const userIndex = users.findIndex(u => u.id === id);
+    if(userIndex == -1){
+        return res.status(404).json({status:"fail"});
+    }
+    users[userIndex] = {
+    ...users[userIndex],
+    ...body
+    };
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        return res.json({status:"success",id:users.length})
+    })
+})
+.delete((req,res)=>{
+    return res.json({status:"pending"});
+})
+
+
 app.post('/api/users',(req,res)=>{
     // Create new user
-    return res.json({status: "pending"});
+    const body = req.body;
+    users.push({...body,id:users.length+1});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        return res.json({status:"success",id:users.length})
+    })
 });
 
 app.listen(PORT,()=>{
